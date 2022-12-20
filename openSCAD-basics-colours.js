e9a572e2b93f1d3d24f8fe00bc53a4a852d67898
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-12-20 23:02:09 +0000

    Add files via upload

---
+++ openSCAD-basics-colours.js
@@ -0,0 +1,40 @@
+document.getElementById("classq").hidden = true
+
+function reveal() {
+    document.getElementById("classq").hidden = false
+    document.getElementById("classbtnshow").hidden = true
+    document.getElementById("classinfo").hidden = true
+}
+
+function mark() {
+    let first = 0
+    let second = 0
+    let third = 0
+
+    let q1 = document.getElementById("q1")
+    let q2 = document.getElementById("q2")
+    let q3 = document.getElementById("q3")
+
+    if (q1.value == "cylinder(5);") {
+        first = 1
+        document.getElementById("extra1").innerText = "You got question 1 correct."
+    } else {
+        document.getElementById("extra1").innerText = "You got question 1 incorrect."
+    }
+
+    if (q2.value == "color([1, 0, 1])") {
+        second = 1
+        document.getElementById("extra2").innerText = "You got question 2 correct."
+    } else {
+        document.getElementById("extra2").innerText = "You got question 2 incorrect."
+    }
+
+    if (q3.value == "cube(5);") {
+        third = 1
+        document.getElementById("extra3").innerText = "You got question 3 correct."
+    } else {
+        document.getElementById("extra3").innerText = "You got question 3 incorrect."
+    }
+
+    document.getElementById("results").innerText = "You got " + parseInt(first + second + third) + "/3"
+}
---
+++ what-is-OpenSCAD.js
@@ -0,0 +1,31 @@
+document.getElementById("classq").hidden = true
+
+function reveal() {
+    document.getElementById("classq").hidden = false
+    document.getElementById("classbtnshow").hidden = true
+    document.getElementById("classinfo").hidden = true
+}
+
+function mark() {
+    let first = 0
+    let second = 0
+
+    let q1 = document.getElementById("q1")
+    let q2 = document.getElementById("q2")
+
+    if (q1.value.toLowerCase().includes("programming")) {
+        first = 1
+        document.getElementById("extra1").innerText = "You got question 1 correct."
+    } else {
+        document.getElementById("extra1").innerText = "You got question 1 incorrect."
+    }
+
+    if (q2.value.toLowerCase().includes("3d printed")) {
+        second = 1
+        document.getElementById("extra2").innerText = "You got question 2 correct."
+    } else {
+        document.getElementById("extra2").innerText = "You got question 2 incorrect."
+    }
+
+    document.getElementById("results").innerText = "You got " + parseInt(first + second) + "/2"
+}
